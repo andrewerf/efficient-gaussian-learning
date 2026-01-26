2328dc207523fcc25fb8da19ec89f7203f5ec610
roseopicta <emilie.o.gillet+github@gmail.com>
2026-01-26 20:40:07 +0100

    Cleanup, refactoring and an ugly brute force diamond norm

---
+++ gaussian.py
@@ -0,0 +1,185 @@
+from dataclasses import dataclass
+
+import numpy as np
+import scipy as sp
+from scipy.stats import multivariate_normal, unitary_group
+
+
+def check_symplectic(S):
+    num_modes = S.shape[0] // 2
+    Omega = np.block(
+        [
+            [np.zeros((num_modes, num_modes)), np.eye(num_modes)],
+            [-np.eye(num_modes), np.zeros((num_modes, num_modes))],
+        ]
+    )
+    symplectic_check = S.T @ Omega @ S
+    d = np.linalg.matrix_norm(symplectic_check - Omega, ord="fro")
+    return np.allclose(symplectic_check, Omega)
+
+
+@dataclass
+class GaussianUnitary:
+    S: np.ndarray
+    d: np.ndarray
+
+    def __post_init__(self):
+        num_modes = self.S.shape[0] // 2
+
+        if self.S.shape != (num_modes * 2, num_modes * 2) or self.d.shape != (
+            2 * num_modes,
+        ):
+            raise ValueError("Invalid dimensions.")
+
+        if not check_symplectic(self.S):
+            raise ValueError("S is not symplectic.")
+        self.num_modes = num_modes
+
+    def apply(self, state: "GaussianState") -> "GaussianState":
+        new_r = self.S @ state.r + self.d
+        new_sigma = self.S @ state.sigma @ self.S.T
+        return GaussianState(sigma=new_sigma, r=new_r)
+
+
+def random_unitary(num_modes, d_scale):
+    U = unitary_group.rvs(num_modes)
+    X = U.real
+    Y = U.imag
+    S = np.block([[X, -Y], [Y, X]])
+    d = np.random.randn(2 * num_modes) * d_scale
+    return GaussianUnitary(S, d)
+
+
+class GaussianState:
+    def __init__(
+        self,
+        num_modes=1,
+        sigma=None,
+        r=None,
+    ):
+        if sigma is not None:
+            num_modes = sigma.shape[0] // 2
+            self.r = r
+            self.sigma = sigma
+        else:
+            self.r = np.zeros(2 * num_modes)
+            self.sigma = np.eye(2 * num_modes)
+        self.num_modes = num_modes
+
+    def set_coherent(self, i, alpha):
+        alpha = complex(alpha)
+        self.r[i] = alpha.real
+        self.r[i + self.num_modes] = alpha.imag
+
+    def set_squeezed_vacuum(self, i, z):
+        self.sigma[i, i] = z
+        self.sigma[i + self.num_modes, i + self.num_modes] = 1 / z
+
+    def _get_indices(self, modes):
+        if modes is None:
+            return np.arange(2 * self.num_modes)
+        modes = np.atleast_1d(modes)
+        return np.concatenate([modes, modes + self.num_modes])
+
+    def transform(self, u, modes=None):
+        if type(u) is np.ndarray:
+            S, d = u, np.zeros((u.shape[0],))
+        else:
+            S, d = u.S, u.d
+        idx = self._get_indices(modes)
+        if S.shape != (len(idx), len(idx)) or d.shape != (len(idx),):
+            raise ValueError("Dimensions of S or d do not match the number of modes.")
+        self.sigma[np.ix_(idx, idx)] = S @ self.sigma[np.ix_(idx, idx)] @ S.T
+        self.r[idx] = S @ self.r[idx] + d
+
+    def sample_heterodyne(self, modes=None, num_samples=1):
+        idx = self._get_indices(modes)
+        n = len(idx)
+        mean = self.r[idx]
+        cov = (self.sigma[np.ix_(idx, idx)] + np.eye(n)) / 2
+        return multivariate_normal.rvs(mean=mean, cov=cov, size=num_samples)
+
+    def sample_homodyne(self, quadrature="x", modes=None, num_samples=1):
+        modes = np.arange(self.num_modes) if modes is None else np.atleast_1d(modes)
+        if quadrature == "x":
+            idx = modes
+        elif quadrature == "p":
+            idx = modes + self.num_modes
+        else:
+            raise ValueError("Quadrature must be 'x' or 'p'")
+
+        mean = self.r[idx]
+        cov = self.sigma[np.ix_(idx, idx)] / 2
+        return multivariate_normal.rvs(mean=mean, cov=cov, size=num_samples)
+
+    @property
+    def mean_photon_number(self):
+        return (
+            0.25 * (np.trace(self.sigma)) + 0.5 * self.r @ self.r - self.num_modes * 0.5
+        )
+
+
+def xpxp_to_xxpp(s):
+    n = s.shape[0]
+    indices = np.arange(n)
+    indices[: n // 2] = indices[::2]
+    indices[n // 2 :] = indices[: n // 2] + 1
+    s = s[indices, :]
+    s = s[:, indices]
+    return s
+
+
+def overlap(psi_1: GaussianState, psi_2: GaussianState):
+    Sigma = psi_1.sigma + psi_2.sigma
+    num_modes = psi_1.sigma.shape[0] // 2
+    try:
+        c, lower = sp.linalg.cho_factor(Sigma, check_finite=True)
+    except np.linalg.LinAlgError as e:
+        raise LinAlgError("Sigma matrix must be positive definite") from e
+
+    d = psi_1.r - psi_2.r
+    inv_Sigma_d = sp.linalg.cho_solve((c, lower), d, check_finite=True)
+    exponent = float(d.T @ inv_Sigma_d)
+    logdet_Sigma = 2.0 * np.sum(np.log(np.diag(c)))
+    overlap_log = -0.5 * (logdet_Sigma + exponent) + num_modes * np.log(2)
+    return np.exp(overlap_log)
+
+
+def random_coherent_state(num_modes, target_mean_photon_number):
+    state = GaussianState(
+        sigma=np.eye(num_modes * 2, num_modes * 2), r=np.random.randn(num_modes * 2)
+    )
+    state.r *= (target_mean_photon_number / state.mean_photon_number) ** 0.5
+    return state
+
+
+def random_state(num_modes, target_mean_photon_number):
+    # Start by applying a random unitary to a vacuum state
+    state = random_unitary(num_modes, 10).apply(GaussianState(num_modes))
+
+    # Kind of hack-ish NLA to reach the target photon number
+    g = np.sqrt(target_mean_photon_number / state.mean_photon_number)
+    r = g * state.r
+    sigma = (g**2 * state.sigma) - ((g**2 - 1) * np.eye(2 * state.num_modes))
+    return GaussianState(sigma=sigma, r=r)
+
+
+def ec_diamond_norm(
+    U: GaussianUnitary,
+    V: GaussianUnitary,
+    max_n=10_000,
+    num_samples=1_000,
+    method="sampling",
+):
+    def _ecd_overlap(U: GaussianUnitary, V: GaussianUnitary, psi: GaussianState):
+        return 2 * (1 - overlap(U.apply(psi), V.apply(psi))) ** 0.5
+
+    num_modes = U.S.shape[0] // 2
+    if method == "sampling":
+        # Evaluate the overlap on randomly generated coherent and random gaussian states
+        c_states = [random_coherent_state(num_modes, max_n) for _ in range(num_samples)]
+        r_states = [random_state(num_modes, max_n) for _ in range(num_samples)]
+        r_states = [r for r in r_states if r.mean_photon_number <= max_n]
+        return np.max([_ecd_overlap(U, V, state) for state in c_states + r_states])
+    else:
+        raise NotImplementedError("Not yet!")
--- main.py
+++ main.py
@@ -1,131 +1,144 @@
 import numpy as np
 import scipy as sc
-from scipy.stats import unitary_group
 from scipy.linalg import block_diag
 
-from baby_gauss import GaussianSimulator, xpxp_to_xxpp
-
-def random_symplectic(n):
-    U = unitary_group.rvs(n)
-    X = U.real
-    Y = U.imag
-    S = np.block([[X, -Y], [Y, X]])
-    assert_symplectic(S)
-    return S
-
-
-def assert_symplectic(S):
-    num_modes = S.shape[0] // 2
-    Omega = np.block([[np.zeros((num_modes, num_modes)), np.eye(num_modes)],
-                      [-np.eye(num_modes), np.zeros((num_modes, num_modes))]])
-    symplectic_check = S.T @ Omega @ S
-    d = np.linalg.matrix_norm(symplectic_check - Omega, ord='fro')
-    assert np.allclose(symplectic_check, Omega), f'Omega L2-distance: {d}'
+from gaussian import (
+    GaussianState,
+    GaussianUnitary,
+    check_symplectic,
+    ec_diamond_norm,
+    random_coherent_state,
+    random_unitary,
+    xpxp_to_xxpp,
+)
 
 
 def symplectic_correction(S):
     num_modes = S.shape[0] // 2
-    Omega = np.block([[np.zeros((num_modes, num_modes)), np.eye(num_modes)],
-                      [-np.eye(num_modes), np.zeros((num_modes, num_modes))]])
+    Omega = np.block(
+        [
+            [np.zeros((num_modes, num_modes)), np.eye(num_modes)],
+            [-np.eye(num_modes), np.zeros((num_modes, num_modes))],
+        ]
+    )
     T = -Omega @ S.T @ Omega @ S
     Q = sc.linalg.sqrtm(T)
     R = S @ np.linalg.inv(Q)
-    assert_symplectic(R)
+    if not check_symplectic(R):
+        raise ValueError("Symplectic correction failed.")
     return R.real
 
 
-def coherent_probe(S, d, eta, mode, num_samples):
-    num_modes = S.shape[0] // 2
-    g = GaussianSimulator(num_modes)
+def coherent_probe(U, eta, mode, num_samples):
+    num_modes = U.num_modes
+    g = GaussianState(num_modes)
     if eta is not None and mode is not None:
         g.set_coherent(mode % num_modes, eta * (1 if mode < num_modes else 1j))
-    g.transform(S, d=d)
+    g.transform(U)
     return g.sample_heterodyne(num_samples=num_samples)
 
 
-def estimate_symplectic(S, d, num_samples, eta, kind="symmetric"):
+def estimate_symplectic(U, num_samples, eta, kind="symmetric"):
     if kind not in ["symmetric", "shared"]:
         raise ValueError("kind must be 'symmetric' or 'shared'")
-    num_modes = S.shape[0] // 2
+    num_modes = U.S.shape[0] // 2
     est_S = np.zeros((num_modes * 2, num_modes * 2))
     scale = 1 if kind == "shared" else 0.5
     if kind == "shared":
-        y_vacuum = np.mean(coherent_probe(S, d, None, None, num_samples), axis=0)
+        y_vacuum = np.mean(coherent_probe(U, None, None, num_samples), axis=0)
 
     for i in range(num_modes * 2):
-        y = coherent_probe(S, d, eta, i, num_samples)
+        y = coherent_probe(U, eta, i, num_samples)
         if kind == "symmetric":
-            y -= coherent_probe(S, d, -eta, i, num_samples)
+            y -= coherent_probe(U, -eta, i, num_samples)
         else:
             y -= y_vacuum
         est_S[:, i] = np.mean(y, axis=0) / eta * scale
     return symplectic_correction(est_S)
 
 
-def single_mode_squeezed_probe(est_S, S, d, z, quadrature, num_samples):
-    num_modes = S.shape[0] // 2
-    g = GaussianSimulator(num_modes)
+def single_mode_squeezed_probe(est_S, U, z, quadrature, num_samples):
+    num_modes = U.num_modes
+    g = GaussianState(num_modes)
     for i in range(num_modes):
         g.set_squeezed_vacuum(i, z)
     g.transform(np.linalg.inv(est_S))
-    g.transform(S, d=d)
+    g.transform(U)
     return g.sample_homodyne(quadrature, num_samples=num_samples)
 
 
-def two_mode_squeezed_probe(est_S, S, d, nu, num_samples):
-    num_modes = S.shape[0] // 2
-    g = GaussianSimulator(num_modes * 2)
+def two_mode_squeezed_probe(est_S, U, nu, num_samples):
+    num_modes = U.num_modes
+    g = GaussianState(num_modes * 2)
 
     Z = block_diag(*[[[1, 0], [0, -1]] for i in range(num_modes)])
-    Snu = np.block([
-        [np.sqrt(nu)*np.eye(num_modes*2), np.sqrt(nu - 1)*Z],
-        [np.sqrt(nu - 1)*Z, np.sqrt(nu)*np.eye(num_modes*2)]
-    ])
+    Snu = np.block(
+        [
+            [np.sqrt(nu) * np.eye(num_modes * 2), np.sqrt(nu - 1) * Z],
+            [np.sqrt(nu - 1) * Z, np.sqrt(nu) * np.eye(num_modes * 2)],
+        ]
+    )
     Snu = xpxp_to_xxpp(Snu)
     g.transform(Snu)
     g.transform(np.linalg.inv(est_S), modes=range(num_modes))
-    g.transform(S, d=d, modes=range(num_modes))
+    g.transform(U, modes=range(num_modes))
     g.transform(np.linalg.inv(Snu))
     return g.sample_heterodyne(num_samples=num_samples, modes=range(num_modes))
 
 
-def estimate_displacement(S, d, num_samples, sqz_param, est_S=None, kind="two_mode"):
+def estimate_displacement(U, num_samples, sqz_param, est_S=None, kind="two_mode"):
     if kind not in ["single_mode", "two_mode"]:
         raise ValueError("kind must be 'single_mode' or 'two_mode'")
 
     if est_S is None:
         est_S = S
     if kind == "two_mode":
-        Y = two_mode_squeezed_probe(est_S, S, d, sqz_param, num_samples).mean(axis=0)
-        return Y / np.sqrt(sqz_param)
+        Y = two_mode_squeezed_probe(est_S, U, sqz_param, num_samples)
+        return Y.mean(axis=0) / np.sqrt(sqz_param)
     else:
-        Yx = single_mode_squeezed_probe(est_S, S, d, 1 / sqz_param, 'x', num_samples).mean(axis=0)
-        Yp = single_mode_squeezed_probe(est_S, S, d, sqz_param, 'p', num_samples).mean(axis=0)
-        return np.concat((Yx, Yp))
+        Yx = single_mode_squeezed_probe(est_S, U, 1 / sqz_param, "x", num_samples)
+        Yp = single_mode_squeezed_probe(est_S, U, sqz_param, "p", num_samples)
+        return np.concat((Yx.mean(axis=0), Yp.mean(axis=0)))
 
 
 def main():
+    C = random_coherent_state(5, 100)
+    print(C)
+
     num_modes = 2
     np.random.seed(42)
 
-    S = random_symplectic(num_modes)
-    d = np.random.randn(2 * num_modes) * 100
+    U = random_unitary(num_modes, 100)
     num_samples = 1000
-
     eta = 100
     sqz = 10
 
-    pred_S_sym = estimate_symplectic(S, d, num_samples, eta, kind="symmetric")
-    print(f'Symplectic Symmetric Estimator: {np.linalg.matrix_norm(S - pred_S_sym, ord="fro")}')
+    def print_cmp(name, A, B):
+        if len(A.shape) == 2:
+            d = np.linalg.matrix_norm(A - B, ord="fro")
+        else:
+            d = np.linalg.norm(A - B)
+        print(f"{name}: {d}")
 
-    pred_S_shared = estimate_symplectic(S, d, num_samples, eta, kind="shared")
-    print(f'Symplectic Shared Estimator: {np.linalg.matrix_norm(S - pred_S_shared, ord="fro")}')
+    est_S_sym = estimate_symplectic(U, num_samples, eta, kind="symmetric")
+    print_cmp("Symplectic symmetric Estimator", est_S_sym, U.S)
 
-    pred_d_sqz = estimate_displacement(S, d, num_samples, sqz, kind="single_mode", est_S=pred_S_sym)
-    print(f'Displacement Singlemode-Squeezed Estimator: {np.linalg.norm(pred_d_sqz - d)}')
+    est_S_shared = estimate_symplectic(U, num_samples, eta, kind="shared")
+    print_cmp("Symplectic shared Estimator", est_S_shared, U.S)
 
-    pred_d_tms = estimate_displacement(S, d, num_samples, sqz, kind="two_mode", est_S=pred_S_sym)
-    print(f'Displacement Aux Estimator: {np.linalg.norm(pred_d_tms - d)}')
+    est_d_sqz = estimate_displacement(
+        U, num_samples, sqz, kind="single_mode", est_S=est_S_sym
+    )
+    print_cmp("Displacement Singlemode-Squeezed Estimator", est_d_sqz, U.d)
+
+    # TODO(emilie): still fails to generate a symmetric p.s.d matrix sometimes
+    est_d_tms = estimate_displacement(
+        U, num_samples, sqz, kind="two_mode", est_S=est_S_sym
+    )
+    print_cmp("Displacement Aux Estimator", est_d_tms, U.d)
+
+    V = GaussianUnitary(est_S_sym, est_d_tms)
+    print("ECD:", ec_diamond_norm(U, V, max_n=1e4, method="sampling"))
 
 
 if __name__ == "__main__":
